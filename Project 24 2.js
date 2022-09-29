04419d5f1449c508f9384724b746763e79094f14
Aakanksha201 <111865622+Aakanksha201@users.noreply.github.com>
2022-09-29 17:14:40 +0530

    Add files via upload

---
+++ Project 24 2.js
@@ -0,0 +1,66 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var btn2;
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+   btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  ground = Bodies.rectangle(100,400,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+ 
+
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,500,20);
+ 
+console.log(ground.position.y);
+
+  
+  
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
+  
